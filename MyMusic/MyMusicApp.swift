50686ab51cc4cf5a7cd0db455f86101aa970704e
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-02 16:31:07 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 戸上健太 on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
